daf27d8ef2465d1482ea011852cc27e3dc26daec
Diaa-E <blarb21@yahoo.com>
2023-11-07 15:23:55 +0200

    Add book title and author fields, add styles

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,27 @@
 import { useState } from "react";
 import "../styles/Form.css";
 import FormButton from "./FormButton";
+import FormInput from "./FormInput";
 
 export default function Form()
 {
     return (
         <>
             <form action="" className="add-book-form">
-                <FormButton className={"add-book-form-button green-button"} text="Add Book" type="submit"/>
-                <FormButton className={"add-book-form-button red-button"} text="Clear"/>
+                <FormInput 
+                    id="book-title" 
+                    required={true}
+                    text="Book Title"
+                    placeHolder="Harry Potter and the Philosopher's Stone"
+                />
+                <FormInput
+                    id="book-author"
+                    required={true}
+                    text="Author"
+                    placeHolder="J. K. Rawling"
+                />
+                <FormButton className={"form-button green-button"} text="Add Book" type="submit"/>
+                <FormButton className={"form-button red-button"} text="Clear"/>
             </form>
         </>
     )
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import "../styles/FormInput.css";
 
-export default function FormInput({id})
+export default function FormInput({id, text="label", placeHolder="Write text here", required = false})
 {
     return (
         <>
-            <label htmlFor={id}></label>
-            <input id={id} type="text" className="add-book-input">
+            <label className="form-label" htmlFor={id}>{text}</label>
+            <input required={required} placeholder={placeHolder} id={id} type="text" className="form-input">
 
             </input>
         </>
